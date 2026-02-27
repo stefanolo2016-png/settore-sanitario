84c5c6da4258b8bf81033a218dcf2a7124a6ac20
stefanolo2016-png <stefano.lo2016@gmail.com>
2026-02-27 19:28:07 +0100

    Update accedi.php

--- accedi.php
+++ accedi.php
@@ -1,84 +1,94 @@
-<?php
 
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>ACCESSO</title>
+    <link rel="stylesheet" href="accedi.css">
+</head>
+<body>
+
+    <div id="box">
+        <h1 id="accesso">ACCESSO</h1>
+
+       
+
+        <form action="accedi.php" method="POST">
+            <label>CE:</label><br>
+            <input type="text" id="ce" name="ce" maxlength="16" required><br>
+            <label>password</label><br>
+            <input type="password" id="password" name="password" required><br><br>
+            <p>entri come Amministratore (admin)?</p>
+            <input type="radio" id="admin_si" name="character" value="1" required>
+            <label for="admin_si">Sì</label>
+
+            <input type="radio" id="admin_no" name="character" value="0">
+            <label for="admin_no">No</label><br><br>
+
+            <button type="submit">invio</button>
+        </form>
+
+     
+        <a id="login" href="login.php"><p>login</p></a>
+        <a id="home" href="index.php"><p>home</p></a>
+
+    </div>
+
+</body>
+</html>
+
+<?php
 session_start();
+
 // 1. Configurazione Database
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password_db = ""; 
 $dbname = "sanitario";
 
 try {
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password_db);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connessione fallita: " . $e->getMessage());
 }
 
+$errore = ""; 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $ce = $_POST['ce'];
+    $password_inserita = $_POST['password'];
+    $scelta_admin = $_POST['character']; 
 
-    $ce=$_POST['ce'];
-    $password=$_POST['password'];
+    $sql = "SELECT * FROM private_area WHERE CE = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$ce]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-   $sql = "SELECT * FROM private_area WHERE ce=? AND password=?";
-   $stmt = $pdo->prepare($sql);
-   $stmt->execute([$ce,$password]);
-
-   
-   
-
-        if ($stmt->rowCount() == 1) {
-   
-        // Se tutto va bene, vai alla pagina accedi.php
-        header("Location: index.php");
-
-        $_SESSION['ce']=$ce;
+    if ($user && password_verify($password_inserita, $user['password'])) {
         
+        // Controllo coerenza Admin
+        if ($scelta_admin == "1" && $user['character'] == "0") {
+            $errore = "Accesso negato: non hai i permessi di Amministratore.";
+        } else {
+            session_regenerate_id();
+            
+            // Salvataggio di TUTTI i dati che ti servono (quelli che avevi prima)
+            $_SESSION['ce'] = $user['ce'];
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['surname'] = $user['surname'];
+            $_SESSION['character'] = $user['character']; 
+            $_SESSION['telephone'] = $user['telephone'];
+            $_SESSION['city'] = $user['city'];
+            $_SESSION['registration'] = $user['registration'];
+            $_SESSION['birth'] = $user['birth'];
+            $_SESSION['place'] = $user['city'] . " " . $user['via'] . " " .$user['civic'];
 
-        exit();
+            header("Location: index.php");
+            exit();
+        }
     } else {
-        // Se l'utente esiste già
-        echo "Errore: l'utente con questo CE e telephone NON ESISTE.";
-
+        $errore = "Errore: CE o password errati.";
     }
-
-   }
+}
 ?>
-<html>
-
-<head>
-
-    <title>ACCESSO</title>
-    <link rel="stylesheet" href="accedi.css">
-
-
-
-</head>
-
-<body>
-
-    <div id="box">
-
-    <h1 id="accesso">ACCESSO</h1>
-
-    <form action="accedi.php" method="POST">
-        <label>CE:</label><br>
-        <input type="text" id="ce" name="ce" maxlength="16" required><br>
-        <label>password</label><br>
-        <input type="password" id="password" name="password"  required><br><br>
-        
-        <button type="submit">invio</button>
-    </form>
-
-    <a id="login" href="login.php"><p>login</p></a>
-    <a id="home" href="index.php"><p>home</p></a>
-
-    </div>
-
-
-
-
-
-</body>
-
-
-</html>
